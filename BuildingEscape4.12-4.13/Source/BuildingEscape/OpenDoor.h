add24393c7820912e17964e44483efe0e890519a
Hexen <hexenskull@gmail.com>
2016-07-04 22:48:05 +0800

    88 Exposing events to blueprint finished. Added ability to make good changes in game attaching c++ code with blueprints. Added class in OpenDoor.h FOnOpenRequest

--- BuildingEscape4.12-4.13/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape4.12-4.13/Source/BuildingEscape/OpenDoor.h
@@ -5,6 +5,8 @@
 #include "Components/ActorComponent.h"
 #include "OpenDoor.generated.h"
 
+//below is macro for blueprint acces to cpp code
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest); //it is like creating a class inside class in braces
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -23,6 +25,10 @@ public:
     
     // Called every frame
     virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+    
+    //below is uproperty for blueprint
+    UPROPERTY(BlueprintAssignable)
+    FOnOpenRequest OnOpenRequest;
     
     
 private:
